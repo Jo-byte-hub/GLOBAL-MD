4c2c123ace7dea1314741b831d0c78f1b9786125
Jo-byte-hub <okoyejoshua91@gmail.com>
2024-09-24 01:00:21 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,23 +5,23 @@ const chalk = require('chalk');
 
 //owmner v card
 global.ytname = "YT: GlobalTechInfo" //ur yt chanel name
-global.socialm = "GitHub: GlobalTechInfo" //ur github or insta name
+global.socialm = "GitHub: Jo-byte-hub" //ur github or insta name
 global.location = "Pakistan, Karachi, Karachi" //ur location
 
 //new
-global.botname = '© 𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃' //ur bot name
-global.ownernumber = ['923444844060'] //ur owner number, dont add more than one
-global.ownername = '© GlobalTechInfo' //ur owner name
-global.websitex = "https://www.youtube.com/@GlobalTechInfo"
-global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
+global.botname = '© JOSHUA' //ur bot name
+global.ownernumber = ['2349115983460'] //ur owner number, dont add more than one
+global.ownername = '© Joshua' //ur owner name
+global.websitex = "https://www.youtube.com/@Josh"
+global.wagc = "https://chat.whatsapp.com/Ek55y9kQcFTKO8BtUT0RfH"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/GlobalTechInfo/GLOBAL-MD' //script link
+global.botscript = 'https://github.com/Jo-byte-hub/GLOBAL-MD' //script link
 global.packname = "Sticker By"
-global.author = "GlobalTechInfo\n\n+923444844060"
-global.creator = "923444844060@s.whatsapp.net"
+global.author = "Joshua\n\n+2349115983460"
+global.creator = "2349115983460@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["923444844060"] // Premium User
+global.premium = ["2349115983460"] // Premium User
 
 //channel id
 global.xchannel = {
@@ -51,7 +51,7 @@ global.uang = {
 
 global.mess = {
 	error: 'Error!',
-	nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
+	nsfw: 'Nsfw is disabled in this group, Please tell my handsome owner(Joshua😘)to enable',
 	done: 'Done'
 }
 
